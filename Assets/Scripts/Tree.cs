6a5c25b9b7ffc8d2a15e708d08c6b936f3452799
GeneralSkeltal <cjv81@scarletmail.rutgers.edu>
2022-12-16 11:54:51 -0500

    added in a skybox, added in paths, successfully created teleportation, solved terrain changing bug.

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -6,15 +6,18 @@ public class Tree : MonoBehaviour
 {
     public Transform teleportTarget;
     public GameObject playerObj;
-
+    public CharacterController controller;
     private void Start()
     {
         playerObj = GameObject.FindGameObjectWithTag("Player");
+        controller = playerObj.GetComponent<CharacterController>();
     }
 
     public void OnInteract()
     {
         Debug.Log("playerTryingToClimbTree");
+        controller.enabled = false;
         playerObj.transform.position = teleportTarget.position;
+        controller.enabled = true;
     }
 }
